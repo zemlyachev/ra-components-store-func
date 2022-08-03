2a8bf7994454d6318868dc0d8ae9ff97b4735002
Andrey Zemlyachev <kvintilian@gmail.com>
2022-08-03 12:52:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Страница интернет-магазина (функциональный компонент)
+
+Решение к уроку "Компоненты"
+
+Задание: [https://github.com/netology-code/ra16-homeworks/tree/master/components/store-func](https://github.com/netology-code/ra16-homeworks/tree/master/components/store-func)
+
+----
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './css/main.css';
+import ShopItemFunc from './components/ShopItemFunc';
+import ItemModel from './models/ItemModel';
+
+function App() {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  };
+
+  let itemModel = new ItemModel();
+  Object.assign(itemModel, item);
+
+  return (
+    <div className="container">
+      <div className="background-element"></div>
+      <div className="highlight-window">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={itemModel} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
